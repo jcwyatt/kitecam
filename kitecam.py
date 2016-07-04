1b1dba3301f7d0a54923205bdbd2719e310e55f5
jcwyatt <jwyatt043@googlemail.com>
2016-07-04 22:05:14 +0000

    Added function to find index of last picture file saved and return the value.

--- kitecam.py
+++ kitecam.py
@@ -1,23 +1,25 @@
 from picamera import PiCamera
 from time import sleep
-import datetime as dt
+#import datetime as dt
 import os
 
-path = "/home/pi/Kitepics/"
-dirs = os.listdir( path )
+def findPrevPic():    #finds the last picture taken and returns the index number
+	
+	path = "/home/pi/Kitecam/Kitepics/"
+	dirs = os.listdir( path )
+	if dirs == []:
+		return(-1)
+	else:
+		lastPicNumber = int(max(dirs)[-8:-4])
+		return (lastPicNumber)
 
-for file in dirs:
-    print file
+startPicNumber = findPrevPic() + 1
 
-'''camera = PiCamera()
+camera = PiCamera()
 camera.resolution = (640, 480)
 
 
+for i in range(startPicNumber,startPicNumber+5):
+    camera.capture('Kitepics/kitecam%04d.jpg'  %i)
+    sleep(10)
 
-laststored = 
-
-for i in range(laststored,):
-    camera.capture('Kitepics/kitecam'+tstamp+'.jpg')
-    print ('captured :' + 'kitecam'+tstamp+'.jpg')
-    sleep(5)
-'''
