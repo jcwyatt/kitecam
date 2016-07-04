d2eca98aaacc03dc65acae42aada49722bea0ae0
jcwyatt <jwyatt043@googlemail.com>
2016-07-04 22:23:59 +0000

    further refinements to make it easy to specify picture interval and number of shots to take

--- kitecam.py
+++ kitecam.py
@@ -3,6 +3,16 @@ from time import sleep
 #import datetime as dt
 import os
 
+camera = PiCamera()
+
+camera.resolution = (2592,1944)
+#camera.resolution = (640,480)
+#camera.resolution = (1024,748)
+
+interval = 10 #interval between shots in seconds
+captures = 60 #number of shots to take before stopping
+
+
 def findPrevPic():    #finds the last picture taken and returns the index number
 	
 	path = "/home/pi/Kitecam/Kitepics/"
@@ -13,13 +23,12 @@ def findPrevPic():    #finds the last picture taken and returns the index number
 		lastPicNumber = int(max(dirs)[-8:-4])
 		return (lastPicNumber)
 
-startPicNumber = findPrevPic() + 1
-
-camera = PiCamera()
-camera.resolution = (640, 480)
 
 
-for i in range(startPicNumber,startPicNumber+5):
+startPicNumber = findPrevPic()+1
+
+
+for i in range(startPicNumber,startPicNumber+captures):
     camera.capture('Kitepics/kitecam%04d.jpg'  %i)
-    sleep(10)
+    sleep(interval)
 
